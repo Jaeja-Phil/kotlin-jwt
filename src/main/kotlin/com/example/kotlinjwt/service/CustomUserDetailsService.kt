6dabc9d1c54283392ceec65d76d4fa158ce9e212
phil <jaeja.phil@gmail.com>
2024-02-06 22:29:27 +0900

    feat(User): Add CustomUserDetailsService
    - create custom UserDetailsService which loads & returns userDetails mapped from user userRepository

---
+++ src/main/kotlin/com/example/kotlinjwt/service/CustomUserDetailsService.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinjwt.service
+
+import com.example.kotlinjwt.repository.UserRepository
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class CustomUserDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+    override fun loadUserByUsername(username: String): UserDetails {
+        return userRepository.findByEmail(username)
+            ?.let {
+                User.builder()
+                    .username(it.username)
+                    .password(it.password)
+                    .roles(it.role.name)
+                    .build()
+            }
+            ?: throw UsernameNotFoundException("User not found")
+    }
+}
